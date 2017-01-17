35810142552034ab1263c616bcad766b832f9fac
Jamie Duncan <jduncan@redhat.com>
2017-01-17 15:43:49 -0500

    one final typo, i hope

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.opentechpodcast.com'
+SITEURL = 'https://www.opentechpodcast.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
